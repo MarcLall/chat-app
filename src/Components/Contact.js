d1523ac76fb842f2b03263227c84fcd33500fd0b
Marc <marclallau@gmail.com>
2021-01-20 12:20:22 +0100

    training contact

---
+++ src/Components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact () {
+    return (
+        <div className="Contact">
+            <img className="avatar" alt="Contact" src="https://randomuser.me/api/portraits/women/71.jpg"/>
+            <div className="status">
+                <div className="name">Elena
+                </div>
+                <div className="status-online">
+                </div>
+                <div  className="status-text">online
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact
