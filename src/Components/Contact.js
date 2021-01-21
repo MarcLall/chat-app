5b89c3725c4b6d7ecf55c46b9bfedfd44d9da2cb
Marc <marclallau@gmail.com>
2021-01-21 17:58:27 +0100

    maj-quest 2.2

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Contact.css'
+import PropTypes from 'prop-types';
 
 function Contact (props) {
     return (
@@ -19,3 +20,9 @@ function Contact (props) {
 }
 
 export default Contact
+
+QuoteCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired
+  };
